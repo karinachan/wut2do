7748886c1b9fb7c67643b598a07bd51e803e4ca9
Vivienne Shaw <vshaw@wellesley.edu>
2014-02-15 11:52:38 -0500

    can search for food

--- bootstrap-3.1.1-dist/form_handle.js
+++ bootstrap-3.1.1-dist/form_handle.js
@@ -12,22 +12,19 @@
 
         var newArray= new Array(partsArray.length); //stores the correctly split but with other chars
 
+		var categoryID = "";
 		
         for (var i=0; i<partsArray.length;i++){
 			var temp = partsArray[i].lastIndexOf("=");
 			var tempstr = partsArray[i].substring(temp+1); 
 			newArray[i] = tempstr; 
-			
-		//	document.write(i); 
-		//	document.w
-		//	console.log(newArray[i]); 
-			
-			//alert(i);
-	//		document.write("<p>"); 
-            //alert("with chars"+newArray[i]);
-      //  }
 		}
 		
+		
+		if(newArray[3] == "food")
+			categoryID = "categoryId=4d4b7105d754a06374d81259&";
+		
+		console.log(newArray[3]); 
 		console.log(newArray[0]);
 		console.log(newArray[1]); 
 	    //document.getElementById("write").innerHTML=document.getElementById("write").innerHTML+newArray[i]; 
@@ -37,7 +34,7 @@
 		document.getElementById("write").innerHTML= document.getElementById("write").innerHTML=document.getElementById("write").innerHTML+"1"+newArray[1];
         alert("here2");*/
 		
-		$.getJSON('https://api.foursquare.com/v2/venues/search?ll=' + newArray[0] + ',' + newArray[1] +
+		$.getJSON('https://api.foursquare.com/v2/venues/search?' + categoryID + 'll=' + newArray[0] + ',' + newArray[1] +
 		'&radius=1000&limit=50&***REMOVED***=***REMOVED***&***REMOVED***=***REMOVED***&v=20140215',
 		
 		function(data) {
@@ -46,10 +43,9 @@
             $(content).appendTo("#names");
         });
     });
-		//var lat = partsArray(
 		
         var resultStrings= newArray.join(",");
     
 	//	document.write(resultStrings); 
     
-     document.getElementById("write").innerHTML=resultStrings;
+    // document.getElementById("write").innerHTML=resultStrings;
