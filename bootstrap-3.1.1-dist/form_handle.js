43d7ff424752400ffd38cb0bba6a7ecf837e509b
Karina Chan <karinachan@outlook.com>
2014-02-15 00:43:03 -0500

    fixed the forms
    again, error with stateless pages…… fucking dumb shit

--- bootstrap-3.1.1-dist/form_handle.js
+++ bootstrap-3.1.1-dist/form_handle.js
@@ -1,29 +1,11 @@
-/* var radio1= document.getElementById("foo");
-alert(radio1);
-var radio2= document.getElementById("foo2");
-alert(radio2);
-var text= document.getElementById("text");
-alert(text);
-var formresult= radio1+radio2+text;
-alert(formresult);
+var raw = window.location.search;
 
-document.getElementById("write").innerHTML = formresult;
-the results of the above come out to null because...as we learned in networks...http requests are stateless and thus the form results from the
-previous page are not preserved. 
+var partsArray = raw.split("=");
+var results= "";
+for (var i = 0; len=partsArray.length; i++){
+    results= results +i;
+}
 
-*/
+document.getElementById("write").innerHTML=results;
 
-/*var result= window.location.search;
 
-document.getElementById("write").innerHTML = window.location.search;
-
-$('#result').html("<br />$('form').serialize():<br />"+ $('form').serialize()+"<br /><br />$('form').serializeArray():<br />" + JSON.stringify($('form').serializeArray()));
-
-document.getElementById("write").innterHTML=$('form').serialize(); */
-
-var result = window.location.search;
-
-//var partsArray = result.split('=');
-//for (int i = 0; i <= partsArray.length; i++) 
-
-document.getElementById("write").innerHTML=result;
