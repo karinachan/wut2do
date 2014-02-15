f042954303201f4dfc2537b6ee24e4149a0b8ac2
Karina Chan <karinachan@outlook.com>
2014-02-14 22:46:58 -0500

    Printing form data still does not work

---
+++ bootstrap-3.1.1-dist/form_handle.js
@@ -0,0 +1,7 @@
+var nameValue = document.getElementById("uniqueID").value;
+var radio1= document.getElementById("foo").value;
+var radio2=document.getElementById("bar").value;
+function saveInfo{ 
+
+document.getElementById("write").innerHTML = nameValue+radio1+radio2;
+}
