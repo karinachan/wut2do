ed5ff8e573bb7fe1d5204cd56271c26aea4110b9
Vivienne Shaw <vshaw@wellesley.edu>
2014-02-15 10:16:57 -0500

    holler big booty in the house
    lol lol lol lol
    also foursq isnt working yet (works on its own not integrated)
    for laura to put in fbook stuff

--- bootstrap-3.1.1-dist/form_handle.js
+++ bootstrap-3.1.1-dist/form_handle.js
@@ -1,3 +1,4 @@
+
  var raw = window.location.search;
         raw= raw.substring(4);
     // alert(raw);
@@ -7,13 +8,38 @@
         //alert(partsArray);
 
         var newArray= new Array(partsArray.length); //stores the correctly split but with other chars
-        for (var i=0; i<partsArray.length;i++){
-            newArray[i]=partsArray[i].split("=");
-            
+
+		
+        for (var i=0; i<partsArray[i].length;i++){
+			var temp = partsArray[i].lastIndexOf("=");
+			var tempstr = partsArray[i].substring(temp+1); 
+			newArray[i] = tempstr; 
+			//alert(i);
+	//		document.write("<p>"); 
             //alert("with chars"+newArray[i]);
-        }
-        
+      //  }
+		}
+		alert("poop");
+	    //document.getElementById("write").innerHTML=document.getElementById("write").innerHTML+newArray[i]; 
+
+		document.getElementById("write").innerHTML= document.getElementById("write").innerHTML=document.getElementById("write").innerHTML+"0"+newArray[0]; 
+		alert("here1");
+		document.getElementById("write").innerHTML= document.getElementById("write").innerHTML=document.getElementById("write").innerHTML+"1"+newArray[1];
+        alert("here2");
+		$.getJSON('https://api.foursquare.com/v2/venues/search?ll=' + 
+		alert(newArray[0]+","+newArray[1]);
+		newArray[0] + ','+newArray[1] + '&radius=1000&limit=50&***REMOVED***=***REMOVED***&***REMOVED***=***REMOVED***&v=20140215',
+		
+		function(data) {
+        $.each(data.response.venues, function(i,venues){
+            content = '<p>' + venues.name + '  ' + venues.location.address + '</p>'; 
+            $(content).appendTo("#names");
+        });
+    });
+		//var lat = partsArray(
+		
         var resultStrings= newArray.join(",");
-        
     
-    document.getElementById("write").innerHTML=resultStrings;
+	//	document.write(resultStrings); 
+    
+     document.getElementById("write").innerHTML=resultStrings;
