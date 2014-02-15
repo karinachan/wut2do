bb6d026c6262913dc360239748a48fea0c6ee234
Karina Chan <karinachan@outlook.com>
2014-02-14 23:07:51 -0500

    So using get
    pros: security doesn’t really matter here…
    we can actually parse it
    maybe step 2: use the mongodb

--- bootstrap-3.1.1-dist/form_handle.js
+++ bootstrap-3.1.1-dist/form_handle.js
@@ -1,7 +1,21 @@
-var nameValue = document.getElementById("uniqueID").value;
-var radio1= document.getElementById("foo").value;
-var radio2=document.getElementById("bar").value;
-function saveInfo{ 
+/* var radio1= document.getElementById("foo");
+alert(radio1);
+var radio2= document.getElementById("foo2");
+alert(radio2);
+var text= document.getElementById("text");
+alert(text);
+var formresult= radio1+radio2+text;
+alert(formresult);
 
-document.getElementById("write").innerHTML = nameValue+radio1+radio2;
-}
+document.getElementById("write").innerHTML = formresult;
+the results of the above come out to null because...as we learned in networks...http requests are stateless and thus the form results from the
+previous page are not preserved. 
+
+*/
+
+var result= window.location.search;
+document.getElementById("write").innerHTML = window.location.search;
+
+$('#result').html("<br />$('form').serialize():<br />"+ $('form').serialize()+"<br /><br />$('form').serializeArray():<br />" + JSON.stringify($('form').serializeArray()));
+
+document.getElementById("write").innterHTML=$('form').serialize(); 
